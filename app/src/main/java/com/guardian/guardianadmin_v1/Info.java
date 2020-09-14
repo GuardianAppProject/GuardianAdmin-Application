3ee4f507b119b095fd816abbe5403ee6fe0e7d34
SinaElahimanesh <59636948+SinaElahimanesh@users.noreply.github.com>
2020-09-14 21:06:58 +0430

    add Menus --> My Account + Info + Support + Setting

--- app/src/main/java/com/guardian/guardianadmin_v1/Info.java
+++ app/src/main/java/com/guardian/guardianadmin_v1/Info.java
@@ -2,7 +2,10 @@ package com.guardian.guardianadmin_v1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Info extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class Info extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        ImageButton backButton = (ImageButton) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Info.this, MainListActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        });
     }
 }
--- app/src/main/java/com/guardian/guardianadmin_v1/Support.java
+++ app/src/main/java/com/guardian/guardianadmin_v1/Support.java
@@ -2,7 +2,13 @@ package com.guardian.guardianadmin_v1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Support extends AppCompatActivity {
 
@@ -10,5 +16,27 @@ public class Support extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_support);
+
+        TextView link = (TextView) findViewById(R.id.websiteText);
+        String linkText = "<a href='http://guardianapp.ir'>وبسایت گاردین</a>";
+        link.setText(Html.fromHtml(linkText));
+        link.setMovementMethod(LinkMovementMethod.getInstance());
+//        // 2) How to place email address
+        TextView email = (TextView) findViewById(R.id.emailText);
+        String emailText = "<a href=\"mailto:support@guardianapp.ir\">ایمیل پشتیبانی</a>";
+        email.setText(Html.fromHtml(emailText));
+        email.setMovementMethod(LinkMovementMethod.getInstance());
+
+
+        ImageButton backButton = (ImageButton) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Support.this, MainListActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        });
     }
 }
