1d08858d5c8edaee653c1281dd1835bfcc8ca65d
SinaElahimanesh <59636948+SinaElahimanesh@users.noreply.github.com>
2020-09-16 21:30:39 +0430

    add DATE button and DATE Picker

--- app/src/main/java/com/guardian/guardianadmin_v1/MainUserActivity.java
+++ app/src/main/java/com/guardian/guardianadmin_v1/MainUserActivity.java
@@ -3,9 +3,18 @@ package com.guardian.guardianadmin_v1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import ir.hamsaa.persiandatepicker.Listener;
+import ir.hamsaa.persiandatepicker.PersianDatePickerDialog;
+import ir.hamsaa.persiandatepicker.util.PersianCalendar;
 
 public class MainUserActivity extends AppCompatActivity {
 
@@ -21,6 +30,39 @@ public class MainUserActivity extends AppCompatActivity {
                 Intent i = new Intent(MainUserActivity.this, MainListActivity.class);
                 startActivity(i);
                 finish();
+            }
+        });
+
+        Button date = findViewById(R.id.date);
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PersianDatePickerDialog picker = new PersianDatePickerDialog(MainUserActivity.this)
+                        .setPositiveButtonString("تایید")
+                        .setNegativeButton("بستن")
+                        .setTodayButton("امروز")
+                        .setTodayButtonVisible(true)
+                        .setMinYear(1300)
+                        .setMaxYear(PersianDatePickerDialog.THIS_YEAR)
+//                        .setInitDate(initDate)
+                        .setActionTextColor(Color.GRAY)
+//                        .setTypeFace(new Typeface())
+                        .setTitleType(PersianDatePickerDialog.WEEKDAY_DAY_MONTH_YEAR)
+                        .setShowInBottomSheet(true)
+                        .setListener(new Listener() {
+                            @Override
+                            public void onDateSelected(PersianCalendar persianCalendar) {
+
+                                Toast.makeText(MainUserActivity.this, persianCalendar.getPersianYear() + "/" + persianCalendar.getPersianMonth() + "/" + persianCalendar.getPersianDay(), Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onDismissed() {
+
+                            }
+                        });
+
+                picker.show();
             }
         });
     }
