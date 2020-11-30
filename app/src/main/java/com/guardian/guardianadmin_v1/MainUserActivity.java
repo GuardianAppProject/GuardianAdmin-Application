1bf5452994e2015107dfd8ba7677909abc9a62d6
SinaElahimanesh <59636948+SinaElahimanesh@users.noreply.github.com>
2020-11-30 20:16:13 +0330

    Gregorian Date

--- app/src/main/java/com/guardian/guardianadmin_v1/MainUserActivity.java
+++ app/src/main/java/com/guardian/guardianadmin_v1/MainUserActivity.java
@@ -63,27 +63,65 @@ public class MainUserActivity extends AppCompatActivity {
         String safety = data[4];
         textView1.setText(safety);
         String speed = data[5];
-        textView2.setText(EncodeDecode.speedDecode(Double.parseDouble(speed)));
+        if(isNumeric(speed)) {
+            textView2.setText(EncodeDecode.speedDecode(Double.parseDouble(speed)));
+        } else {
+            textView2.setText(speed);
+        }
         String nonstop = data[6];
-        textView3.setText(EncodeDecode.withoutStopDecode(Double.parseDouble(nonstop)));
+        if(isNumeric(nonstop)) {
+            textView3.setText(EncodeDecode.withoutStopDecode(Double.parseDouble(nonstop)));
+        } else {
+            textView3.setText(nonstop);
+        }
         String vibration = data[7];
-        textView4.setText(EncodeDecode.vibrationDecode(Double.parseDouble(vibration)));
+        if(isNumeric(vibration)) {
+            textView4.setText(EncodeDecode.vibrationDecode(Double.parseDouble(vibration)));
+        } else {
+            textView4.setText(vibration);
+        }
         String sleep = data[8];
-        textView5.setText(EncodeDecode.sleepDecode(Double.parseDouble(sleep)));
+        if(isNumeric(sleep)) {
+            textView5.setText(EncodeDecode.sleepDecode(Double.parseDouble(sleep)));
+        } else {
+            textView5.setText(sleep);
+        }
         String acceleration = data[9];
-        textView6.setText(EncodeDecode.sleepDecode(Double.parseDouble(acceleration)));
+        if(isNumeric(acceleration)) {
+            textView6.setText(EncodeDecode.sleepDecode(Double.parseDouble(acceleration)));
+        } else {
+            textView6.setText(acceleration);
+        }
         String time = data[10];
-        textView7.setText(EncodeDecode.timeDecode(Double.parseDouble(time)));
+        if(isNumeric(time)) {
+            textView7.setText(EncodeDecode.timeDecode(Double.parseDouble(time)));
+        } else {
+            textView7.setText(time);
+        }
         String danger_zone = data[11];
-        textView8.setText(EncodeDecode.nearCitiesDecode(Double.parseDouble(danger_zone)));
+        if(isNumeric(danger_zone)) {
+            textView8.setText(EncodeDecode.nearCitiesDecode(Double.parseDouble(danger_zone)));
+        } else {
+            textView8.setText(danger_zone);
+        }
         String weather = data[12];
-        textView9.setText(EncodeDecode.weatherDecode(Double.parseDouble(weather)));
+        if(isNumeric(weather)) {
+            textView9.setText(EncodeDecode.weatherDecode(Double.parseDouble(weather)));
+        } else {
+            textView9.setText(weather);
+        }
         String road_type = data[13];
-        textView10.setText(EncodeDecode.roadTypeDecode(Double.parseDouble(road_type)));
+        if(isNumeric(road_type)) {
+            textView10.setText(EncodeDecode.roadTypeDecode(Double.parseDouble(road_type)));
+        } else {
+            textView10.setText(road_type);
+        }
         String traffic = data[14];
-        textView11.setText(EncodeDecode.monthDecode(Double.parseDouble(traffic)));
-
-
+        if(isNumeric(traffic)) {
+            textView11.setText(EncodeDecode.monthDecode(Double.parseDouble(traffic)));
+        } else {
+            textView11.setText(traffic);
+        }
 
 
         Button date = findViewById(R.id.date);
@@ -111,6 +149,10 @@ public class MainUserActivity extends AppCompatActivity {
                                 primeCalendar.set(Calendar.DAY_OF_MONTH,persianCalendar.getPersianDay());
                                 primeCalendar.set(Calendar.MONTH,persianCalendar.getPersianMonth());
                                 primeCalendar.set(Calendar.YEAR,persianCalendar.getPersianYear());
+
+                                // Gregorian
+                                JalaliCalendar.jalaliToGregorian(new JalaliCalendar.YearMonthDate(Calendar.YEAR, Calendar.MONTH, Calendar.DAY_OF_MONTH));
+
                                 System.err.println(persianCalendar.getPersianMonthName());
                                 System.err.println("===========:::");
                                 System.err.println(primeCalendar.toCivil().getMonth()+primeCalendar.toCivil().getDayOfMonth()+primeCalendar.toCivil().getYear());
@@ -132,5 +174,17 @@ public class MainUserActivity extends AppCompatActivity {
         data = newData;
         //inja ans.split dare az onvar miad, chizmiz haro tike tike mikonim set mikonim roye textbox haye xml
 
+    }
+
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
     }
 }
